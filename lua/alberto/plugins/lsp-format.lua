1ed2579db0a7c459bf14d987c70af90639cc0572
Alberto Sartori <alberto.sartori86@gmail.com>
2025-04-02 22:52:40 +0200

    add mason and lsp-format

---
+++ lua/alberto/plugins/lsp-format.lua
@@ -0,0 +1,9 @@
+return {
+    "lukas-reineke/lsp-format.nvim",
+    config = function()
+        require("lsp-format").setup {}
+        local lspcfg = require("lspconfig")
+        lspcfg.lua_ls.setup { on_attach = require("lsp-format").on_attach }
+        lspcfg.bashls.setup { on_attach = require("lsp-format").on_attach }
+    end
+}
