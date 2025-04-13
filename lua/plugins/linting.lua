cc4cb779ffd1802df9ce39361d1fe947b8cc329a
Alberto Sartori <alberto.sartori86@gmail.com>
2025-04-14 00:06:34 +0200

    blink

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"mfussenegger/nvim-lint",
+		lazy = true,
+		event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				go = { "golangcilint" },
+			}
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					-- try_lint without arguments runs the linters defined in `linters_by_ft`
+					-- for the current filetype
+					require("lint").try_lint()
+
+					-- You can call `try_lint` with a linter name or a list of names to always
+					-- run specific linters, independent of the `linters_by_ft` configuration
+					-- require("lint").try_lint("cspell")
+					vim.keymap.set("n", "<leader>li", function()
+						lint.try_lint()
+					end, { desc = "Trigger linting for current file" })
+				end,
+			})
+		end,
+	},
+}
